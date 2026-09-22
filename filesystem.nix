e7319257b0370a27c581fd1f8cc7eebbbd22040f
SixSlime <ryan.mm.taylor@gmail.com>
2026-09-21 18:20:13 -0700

    persist /etc/ssh

--- filesystem.nix
+++ filesystem.nix
@@ -51,6 +51,7 @@ in
       "/var/lib/bluetooth"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
+      "/etc/ssh"
       { directory = "/trash"; mode = "0777"; }
     ];
     files = [
