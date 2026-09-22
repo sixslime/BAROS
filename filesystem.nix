41b2f5d3822c99eba47785eccc9ca4e8a6c010c0
SixSlime <ryan.mm.taylor@gmail.com>
2026-09-21 18:42:48 -0700

    persist ssh keys

--- filesystem.nix
+++ filesystem.nix
@@ -51,11 +51,14 @@ in
       "/var/lib/bluetooth"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      "/etc/ssh"
       { directory = "/trash"; mode = "0777"; }
     ];
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
     users.six = {
       directories = [
