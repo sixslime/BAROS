ffe700e376325bb212e2d7964977a0a14c106b04
SixSlime <ryan.mm.taylor@gmail.com>
2026-09-22 20:29:29 -0700

    -

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,15 +24,16 @@
       open = false;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
       nvidiaSettings = true;
-    };
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
       };
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
     };
+    
   };
   services.xserver.videoDrivers = [ "nvidia" ];
 
